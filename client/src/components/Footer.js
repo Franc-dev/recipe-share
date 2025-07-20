fbf8c30f942f1ab394aa85aa885e793d9d20a5f0
Franc-dev <francismwanik254@gmail.com>
2025-07-20 16:23:34 +0300

    few fixes'

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -90,7 +90,7 @@ const Footer = () => {
         {/* Bottom Bar */}
         <div className="border-t border-gray-700 mt-8 pt-8 flex flex-col md:flex-row justify-between items-center">
           <p className="text-gray-400 text-sm">
-            © 2024 RecipeShare. All rights reserved.
+            © 2025 RecipeShare. All rights reserved.
           </p>
           <div className="flex items-center space-x-4 mt-4 md:mt-0">
             <Link to="/privacy" className="text-gray-400 hover:text-white text-sm transition-colors">
